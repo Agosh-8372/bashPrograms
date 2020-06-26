9a2d3696fb603475f94de8f039cb2ce719b82d29
agosh-8372 <agoshchauhan378@gmail.com>
2020-06-26 17:54:12 +0530

    UC-3:Part-time Employee & Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-random=$((RANDOM%2))
-Wage_perHour=20
-full_dayHrs=8
-if [ $random -eq 1 ]
-then
-echo "Employee is present "
-echo "salary = $((Wage_perHour * full_dayHrs))"
-else
-echo "Employee is absent "
-echo "salary = 0"
-fi
+random=$((RANDOM%3))
+ Wage_perHour=20
+ full_dayHrs=8
+ part_dayHrs=8
+    if [ $random -eq 1 ]
+    then
+    echo "Employee is present and is full time "
+    echo "salary = $((Wage_perHour * full_dayHrs))"
+      elif [ $random -eq 2 ]
+      then
+      echo "Employee is present and is part-time "
+      echo "salary = $((Wage_perHour * part_dayHrs))"
+        else
+        echo "Employee is absent"
+        echo "salary = 0"
+        fi
