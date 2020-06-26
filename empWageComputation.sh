cdc1e4fb00c51b9130397706f0f8688c6fdb5a41
agosh-8372 <agoshchauhan378@gmail.com>
2020-06-26 17:15:10 +0530

    UC-1 Employee:Present or Absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+random=$((RANDOM%2))
+if [ $random -eq 0 ]
+then
+echo "Employee is absent "
+else
+echo "Employee is present "
+fi
