785393d29e21dfb182de9e62fefa9e9772fb07fb
agosh-8372 <agoshchauhan378@gmail.com>
2020-06-26 20:41:49 +0530

    UC-4:Using Switch Case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,17 +2,17 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 random=$((RANDOM%3))
  Wage_perHour=20
- full_dayHrs=8
- part_dayHrs=8
-    if [ $random -eq 1 ]
-    then
-    echo "Employee is present and is full time "
-    echo "salary = $((Wage_perHour * full_dayHrs))"
-      elif [ $random -eq 2 ]
-      then
-      echo "Employee is present and is part-time "
-      echo "salary = $((Wage_perHour * part_dayHrs))"
-        else
-        echo "Employee is absent"
-        echo "salary = 0"
-        fi
+ IS_FULL_TIME=1;
+ IS_PART_TIME=2;
+    case  $random in 
+            $IS_FULL_TIME)
+               empHrs=8;
+		;;
+   	    $IS_PART_TIME)
+               empHrs=4
+                ;;
+              *)
+               empHrs=0
+                  ;;
+    esac
+totalSalary=$((Wage_perHour * empHrs))
