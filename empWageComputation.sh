eb8adc3c369e9b824e640251b6602eeaa78e311d
agosh-8372 <agoshchauhan378@gmail.com>
2020-06-27 16:21:31 +0530

    UC-7:Refactor the code-write function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,27 +1,34 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-IS_PART_TIME=1;
-IS_FULL_TIME=2;
-MAX_MONTH_HR=100;
-EMP_RATE_PER_HR=20;
-NUM_WORKING_DAYS=20;
-   totalEmpHrs=0;
-   totalWorkingDays=0;
-while [[ $totalEmpHrs -le  $MAX_MONTH_HR && $totalWorkingDays -le $NUM_WORKING_DAYS ]]
-do
-    ((totalWorkingDays++))
-   random=$((RANDOM%3))
-    case  $random in
+ WAGE_PER_HR=20;
+ IS_FULL_TIME=1;
+ IS_PART_TIME=2;
+ NUM_WORK_DAYS=20;
+ MAX_MONTH_HR=100;
+totalEmpHrs=0;
+totalWorkingDays=0;
+ function getWorkHrs(){
+   local empCheck=$1;
+     case  $empCheck in
             $IS_FULL_TIME)
                empHrs=8;
-		;;
-   	    $IS_PART_TIME)
+                ;;
+            $IS_PART_TIME)
                empHrs=4
                 ;;
               *)
                empHrs=0
                   ;;
     esac
+   echo $empHrs
+}
+while [[ $totalEmpHrs -le $MAX_MONTH_HR && $totalWorkingDays -le $NUM_WORK_DAYS ]]
+do
+((totalWorkingDays++))
+empCheck=$((RANDOM%3))
+empHrs=$( getWorkHrs $empCheck)
 totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
 done
-totalSalary=$(( $totalEmpHrs*$EMP_RATE_PER_HR ))
+totalSalary=$(( $WAGE_PER_HR * $totalEmpHrs ))
+echo $totalSalary
+
