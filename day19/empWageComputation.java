52c644ef486faffdcdadb1f39027a91a1dac578c
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-04 08:37:16 +0530

    check Employee is present or Absent

--- day19/empWageComputation.java
+++ day19/empWageComputation.java
@@ -1,6 +1,12 @@
 public class empWageComputation{
     public static void main(String[] args){
    System.out.println("Welcome to Employee wage computation program ");
+    int empCheck=(int)Math.floor(Math.random()*10)%2;
+   if(empCheck==0)
+      System.out.println("Employee is absent");
+    else 
+       System.out.println("Employee is present");
+
 }
 }
 
